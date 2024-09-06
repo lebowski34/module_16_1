ff1c0c119ac2d8e0ea1705c0280b63f197c9c90a
lebowski34 <167763740+lebowski34@users.noreply.github.com>
2024-09-06 14:01:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Главная страница"}
+
+@app.get("/user/admin")
+def read_admin():
+    return {"message": "Вы вошли как администратор"}
+
+@app.get("/user/{user_id}")
+def read_user(user_id: int):
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+@app.get("/user")
+def read_user_info(username: str, age: int):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
